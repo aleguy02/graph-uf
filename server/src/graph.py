e784514696efc92a40b9d0423d5dfc6c23590d6c
aleguy02 <alejandro.the.villate@gmail.com>
2025-08-03 17:34:08 -0400

    Implement prev unlocks
    This isn't completed but I have to go and want to push rq

--- server/src/graph.py
+++ server/src/graph.py
@@ -22,7 +22,7 @@ class Graph:
     def getAdjList(self):
         return self.adj_list
 
-    def postreqs(self, root: str, semester: str):
+    def postreqs(self, root: str, semester: str) -> set[str]:
         """
         BFS traversal from root to all reachable nodes.
         Returns all classes that require root as in their prerequisite chain
