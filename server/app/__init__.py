6a2c6615664c5411f56f383705b7900b0ffe8d15
Kevin Pugliese <puglord638@gmail.com>
2025-08-09 00:35:51 -0400

    changed semester selector to be non-abbreviated

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,6 +3,16 @@ from app.routes.main import main_bp
 from app.routes.api import api_bp
 from pathlib import Path
 from src.loader import build_graph, build_tcm, build_tooltip, semesters
+import re
+
+def semester_label(code: str) -> str:
+    m = re.fullmatch(r'(f|sp|sm)(\d{2})', code)
+    if not m:
+        return code #fallback for if not pattern match
+    term, yy = m.groups()
+    term_map = {'f': 'Fall', 'sp': 'Spring', 'sm': 'Summer'}
+    year = 2000 + int(yy)
+    return f"{term_map[term]} {year}"
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -23,6 +33,7 @@ def create_app(test_config=None):
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp)
 
+    app.jinja_env.filters["semester_label"] = semester_label
     @app.errorhandler(404)
     def not_found(e):
         return render_template("404.html", title="GraphUF"), 404
