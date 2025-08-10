ae637ef4b908aa31927a8731db54c36aab7176fa
aleguy02 <alejandro.the.villate@gmail.com>
2025-08-10 01:40:32 -0400

    Strengthen test_factory.py

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,18 +3,14 @@ Test for Flask app factory pattern
 """
 
 from app import create_app
+from src.graph import Graph
+from src.tcm import TCM
 
 
 def test_config():
     assert not create_app().testing
     testing_app = create_app({"TESTING": True})
     assert testing_app.testing
-    assert (
-        testing_app.config["COURSE_GRAPH"]
-        and type(testing_app.config["COURSE_GRAPH"]).__name__ == "Graph"
-    )
-    assert (
-        testing_app.config["COURSE_TCM"]
-        and type(testing_app.config["COURSE_TCM"]).__name__ == "TCM"
-    )
+    assert isinstance(testing_app.config["COURSE_GRAPH"], Graph)
+    assert isinstance(testing_app.config["COURSE_TCM"], TCM)
     assert testing_app.config["TOOLTIP_INFO"]
